96e03f0843d1a47598cefb0c789ef6e1482e7434
luv-mirinae <luv_mirinae@proton.me>
2024-07-31 17:14:26 +0900

    Initial commit

---
+++ src/main/java/im/flos/webapp/FlosWebappApplication.java
@@ -0,0 +1,13 @@
+package im.flos.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlosWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlosWebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/im/flos/webapp/FlosWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package im.flos.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlosWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
