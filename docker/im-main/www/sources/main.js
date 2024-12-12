7667841fe49592a11740277c227b2a5c33b980a3
luv-mirinae <167957329+luv-mirinae@users.noreply.github.com>
2024-12-12 05:57:08 +0000

    Update static web files of im-main

--- docker/im-main/www/sources/main.js
+++ docker/im-main/www/sources/main.js
@@ -1,1 +1,7 @@
-console.log('It works!');
+const blogButtonClick = () => {
+  window.open('https://blog.flos.im', '_blank');
+};
+
+const devButtonClick = () => {
+  window.open('https://my.flos.im', '_blank');
+};
